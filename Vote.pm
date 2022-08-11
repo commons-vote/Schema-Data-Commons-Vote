9ce19162cee8ff51e1cdc94c780bb88252987475
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-08-11 14:21:29 +0200

    Rewrite to use of Schema::Data

--- Vote.pm
+++ Vote.pm
@@ -4,74 +4,17 @@ use base qw(Schema::Data);
 use strict;
 use warnings;
 
-use Class::Utils qw(set_params);
-use English;
-use Error::Pure qw(err);
-use Schema::Commons::Vote;
 use Unicode::UTF8 qw(decode_utf8);
 
 our $VERSION = 0.01;
 
 sub new {
-	my ($class, @params) = @_;
+	my ($class, %params) = @_;
 
-	# Create object.
-	my $self = bless {}, $class;
-
-	# db user password.
-	$self->{'db_password'} = undef;
-
-	# db user name.
-	$self->{'db_user'} = undef;
-
-	# db options.
-	$self->{'db_options'} = {};
-
-	# DSN.
-	$self->{'dsn'} = undef;
-
-	# Process parameters.
-	set_params($self, @params);
-
-	if (! defined $self->{'dsn'}) {
-		err "Parameter 'dsn' is required.";
-	}
-	$self->{'_schema'} = eval {
-		Schema::Commons::Vote->connect(
-			$self->{'dsn'},
-			$self->{'user'},
-			$self->{'password'},
-			$self->{'db_options'},
-		);
-	};
-	if ($EVAL_ERROR) {
-		err 'Cannot connect to Schema database.',
-			'Error', $EVAL_ERROR,
-		;
-	}
+	$params{'schema_module'} = 'Schema::Commons::Vote';
+	my $self = $class->SUPER::new(%params);
 
 	return $self;
-}
-
-sub insert {
-	my $self = shift;
-
-	my $data_hr = $self->data;
-	foreach my $source ($self->{'_schema'}->sources) {
-		my $rs = $self->{'_schema'}->source($source);
-
-		# Skip source table without data.
-		if (! exists $data_hr->{$rs->name}) {
-			next;
-		}
-
-		# Insert.
-		foreach my $data_hr (@{$data_hr->{$rs->name}}) {
-			$self->{'_schema'}->resultset($source)->create($data_hr);
-		}
-	}
-
-	return;
 }
 
 sub data {

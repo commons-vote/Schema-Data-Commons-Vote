d6180504747327596ecd896e30438659382538e5
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-09 18:05:01 +0200

    Update to data with variables
    And add creator of records

--- Vote/0_1_0.pm
+++ Vote/0_1_0.pm
@@ -18,6 +18,8 @@ sub new {
 }
 
 sub data {
+	my ($self, $variables_hr) = @_;
+
 	return {
 		'_order' => [],
 
@@ -25,58 +27,85 @@ sub data {
 			'active' => 1,
 			'hash_type_id' => 1,
 			'name' => 'sha_384',
+			'created_by_id' => 1,
+		}],
+
+		'person' => [{
+			'person_id' => 1,
+			'email' => $variables_hr->{'creator_email'},
+			'name' => $variables_hr->{'creator_name'},
 		}],
 
 		'role' => [{
 			'description' => 'Admin role.',
 			'name' => 'admin',
 			'role_id' => 1,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Jury member role.',
 			'name' => 'jury_member',
 			'role_id' => 2,
+			'created_by_id' => 1,
 		}],
 
 		'validation_type' => [{
 			'description' => 'Checks number of photos by author.',
 			'type' => 'check_author_photos',
 			'validation_type_id' => 1,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if the dimensions are greater than defined.',
 			'type' => 'check_image_dimension',
 			'validation_type_id' => 2,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if the smaller dimension is greater than defined.',
 			'type' => 'check_image_dimensions_short',
 			'validation_type_id' => 3,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if each image is in one section.',
 			'type' => 'check_image_in_one_section',
 			'validation_type_id' => 4,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image size is greater than defined.',
 			'type' => 'check_image_size',
 			'validation_type_id' => 5,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image was created between defined dates.',
 			'type' => 'check_image_created',
 			'validation_type_id' => 6,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image was uploaded between defined dates.',
 			'type' => 'check_image_uploaded',
 			'validation_type_id' => 7,
+			'created_by_id' => 1,
 		}],
 
 		'vote_type' => [{
 			'description' => 'Voting type for jury marking.',
 			'type' => 'jury_marking',
 			'vote_type_id' => 1,
+			'created_by_id' => 1,
 		}, {
 			'description' => 'Voting type for public voting.',
 			'type' => 'public_voting',
 			'vote_type_id' => 2,
+			'created_by_id' => 1,
 		}],
 	};
+}
+
+sub required_variables {
+	my $self = shift;
+
+	return (
+		'creator_name',
+		'creator_email',
+	);
 }
 
 1;

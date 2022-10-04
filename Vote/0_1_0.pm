64406a114c88093921716c0879a0deba09b1c679
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-10-04 20:54:55 +0200

    Remove autoincremented ids

--- Vote/0_1_0.pm
+++ Vote/0_1_0.pm
@@ -27,13 +27,11 @@ sub data {
 
 		'hash_type' => [{
 			'active' => 1,
-			'hash_type_id' => 1,
 			'name' => 'sha_384',
 			'created_by_id' => 1,
 		}],
 
 		'person' => [{
-			'person_id' => 1,
 			'email' => $variables_hr->{'creator_email'},
 			'name' => $variables_hr->{'creator_name'},
 			'wm_username' => $variables_hr->{'creator_wm_username'},
@@ -42,61 +40,50 @@ sub data {
 		'role' => [{
 			'description' => 'Admin role.',
 			'name' => 'admin',
-			'role_id' => 1,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Jury member role.',
 			'name' => 'jury_member',
-			'role_id' => 2,
 			'created_by_id' => 1,
 		}],
 
 		'validation_type' => [{
 			'description' => 'Checks number of photos by author.',
 			'type' => 'check_author_photos',
-			'validation_type_id' => 1,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if the dimensions are greater than defined.',
 			'type' => 'check_image_dimension',
-			'validation_type_id' => 2,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if the smaller dimension is greater than defined.',
 			'type' => 'check_image_dimensions_short',
-			'validation_type_id' => 3,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if each image is in one section.',
 			'type' => 'check_image_in_one_section',
-			'validation_type_id' => 4,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image size is greater than defined.',
 			'type' => 'check_image_size',
-			'validation_type_id' => 5,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image was created between defined dates.',
 			'type' => 'check_image_created',
-			'validation_type_id' => 6,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Checks if image was uploaded between defined dates.',
 			'type' => 'check_image_uploaded',
-			'validation_type_id' => 7,
 			'created_by_id' => 1,
 		}],
 
 		'vote_type' => [{
 			'description' => 'Voting type for jury marking.',
 			'type' => 'jury_marking',
-			'vote_type_id' => 1,
 			'created_by_id' => 1,
 		}, {
 			'description' => 'Voting type for public voting.',
 			'type' => 'public_voting',
-			'vote_type_id' => 2,
 			'created_by_id' => 1,
 		}],
 	};
